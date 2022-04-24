9a2b64d9f7bbdc6aafeb7b21fe5fdefa89a32e79
Sarah Kimak <sarahekimak@gmail.com>
2022-04-24 15:20:19 -0400

    Some stylistic changes to header, a welcome message modal rather than it displaying above the table, moving the edit switch into a column as part of the semester view tables so each course can be individually edited, trying to make add course and semester buttons appear though they are in the components in full

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -27,76 +27,74 @@ export function AddCourse({
         handleClose();
     }
     return (
-        <div>
-            <Modal show={show} onHide={handleClose} animation={false}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Add A Course</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form.Group controlId="AddCourse" as={Row}>
-                        <Form.Label column sm={3}>
-                            Course Code:
-                        </Form.Label>
-                        <Col>
-                            <Form.Control
-                                value={code}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) => setCode(event.target.value)}
-                            />
-                        </Col>
-                        <Form.Label column sm={3}>
-                            Course Title:
-                        </Form.Label>
-                        <Col>
-                            <Form.Control
-                                value={title}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) => setTitle(event.target.value)}
-                            />
-                        </Col>
-                        <Form.Label column sm={3}>
-                            Course Description:
-                        </Form.Label>
-                        <Col>
-                            <Form.Control
-                                value={description}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) => setDescription(event.target.value)}
-                            />
-                        </Col>
-                        <Form.Label column sm={3}>
-                            Course Credits:
-                        </Form.Label>
-                        <Col>
-                            <Form.Control
-                                value={credits}
-                                onChange={(
-                                    event: React.ChangeEvent<HTMLInputElement>
-                                ) => setCredits(event.target.value)}
-                            />
-                        </Col>
-                    </Form.Group>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button
-                        variant="seconary"
-                        onClick={handleClose}
-                        data-testid="cancel-button"
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        variant="primary"
-                        onClick={saveChanges}
-                        data-testid="save-button"
-                    >
-                        Save
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
+        <Modal show={show} onHide={handleClose} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add A Course</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group controlId="AddCourse" as={Row}>
+                    <Form.Label column sm={3}>
+                        Course Code:
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            value={code}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setCode(event.target.value)}
+                        />
+                    </Col>
+                    <Form.Label column sm={3}>
+                        Course Title:
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            value={title}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setTitle(event.target.value)}
+                        />
+                    </Col>
+                    <Form.Label column sm={3}>
+                        Course Description:
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            value={description}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setDescription(event.target.value)}
+                        />
+                    </Col>
+                    <Form.Label column sm={3}>
+                        Course Credits:
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            value={credits}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) => setCredits(event.target.value)}
+                        />
+                    </Col>
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="seconary"
+                    onClick={handleClose}
+                    data-testid="cancel-button"
+                >
+                    Cancel
+                </Button>
+                <Button
+                    variant="primary"
+                    onClick={saveChanges}
+                    data-testid="save-button"
+                >
+                    Save
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
