cd2aacc376f709ab0d14b8ca3338f0e04028c628
Sarah Kimak <sarahekimak@gmail.com>
2022-05-16 23:35:40 -0400

    course display test

--- src/tests/Courses.test.tsx
+++ src/tests/Courses.test.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { CatalogCourses } from "../components/CatalogCourses";
-//import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 describe("Course tests", () => {
     test("Courses have a dropdown", () => {
         render(<CatalogCourses />);
         expect(screen.getByRole("combobox")).toBeInTheDocument();
     });
-    /*test("Selected courses are displayed", () => {
+    test("Selected courses are displayed", () => {
         render(<CatalogCourses />);
         const selected = screen.getByRole("combobox");
         userEvent.selectOptions(selected, "CISC 101");
         expect(
-            screen.getByText(/Title: Principles of Computing/i)
+            screen.getByText(/Principles of Computing/i)
         ).toBeInTheDocument();
-        expect(screen.getByText(/Credits: 3/i)).toBeInTheDocument();
-    });*/
+    });
     test("Courses can be deleted", () => {
         render(<CatalogCourses />);
         const deleteButton = screen.getByRole("button", {
