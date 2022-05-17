eb262c354781a1aa14c5d04729d551bad4d3ec61
Sarah Kimak <sarahekimak@gmail.com>
2022-05-17 16:05:07 -0400

    deleting random semi colon in semester display :)

--- src/components/SemesterDisplay.tsx
+++ src/components/SemesterDisplay.tsx
@@ -118,7 +118,6 @@ export function SemesterDisplay({
                 degreeplanList={degreeplanList}
                 setDegreeplanList={setDegreeplanList}
             ></AddCourse>
-            ;
         </Container>
     );
 }
